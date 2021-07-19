850edf2a854662372f86239a59494efdfe23b215
Eric Stiens <estiens@users.noreply.github.com>
2021-07-19 13:06:32 -0500

    add users and sketchy create user example to show session use from controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def index
+    @user = User.all
+  end
+
+  def create
+    puts params
+    @user = User.create(name: params.dig(:user, :name))
+    session[:user_id] = @user.id if @user.save
+
+    redirect_to root_path
+  end
+end
